a49b7badfcc243403e611aecad2e35d3dce76d92
alexandruvucolov <122124724+alexandruvucolov@users.noreply.github.com>
2025-02-14 00:47:36 +0200

    Update serverClient.ts

--- src/serverClient.ts
+++ src/serverClient.ts
@@ -1,10 +1,9 @@
 import { StreamChat } from 'stream-chat';
 
-export const apiKey = process.env.STREAM_API_KEY as string;
-export const apiSecret = process.env.STREAM_API_SECRET as string;
+export const apiKey = process.env.STREAM_API_KEY;
+export const apiSecret = process.env.STREAM_API_SECRET;
 
 
-console.log(STREAM_API_KEY);
 
 if (!apiKey || !apiSecret) {
   throw new Error(
