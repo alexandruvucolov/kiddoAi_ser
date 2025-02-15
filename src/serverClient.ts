eb5e09ea1253426db28bdae846c8eaf67b4bfa2d
alexandruvucolov <122124724+alexandruvucolov@users.noreply.github.com>
2025-02-15 16:06:12 +0200

    lsls

--- src/serverClient.ts
+++ src/serverClient.ts
@@ -1,13 +1,12 @@
-
 import { StreamChat } from 'stream-chat';
+
+require('dotenv').config();
 
 export const apiKey = process.env.STREAM_API_KEY as string;
 export const apiSecret = process.env.STREAM_API_SECRET as string;
 
 // export const apiKey = "5svvkruencpb";
 // export const apiSecret = "q5nvjxd46dpnk8rp7eu45ehqag8qurfyfeja6umjfst9vmtjn8jzj9y95f3z2mag";
-
-
 
 if (!apiKey || !apiSecret) {
   throw new Error(
